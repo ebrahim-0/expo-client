57e001223a80ea11ac5d39b9a1fdf0295294a0b8
Ebrahim Abd EL Razik <ebrahimabdelrazik2002@gmail.com>
2024-03-15 04:24:06 +0200

    Fix router link in not-founded component

--- src/app/Components/not-founded/not-founded.component.ts
+++ src/app/Components/not-founded/not-founded.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-founded',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './not-founded.component.html',
-  styleUrl: './not-founded.component.css'
+  styleUrl: './not-founded.component.css',
 })
-export class NotFoundedComponent {
-
-}
+export class NotFoundedComponent {}
